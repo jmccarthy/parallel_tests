f156a3a612ce3552e214a2cb1bae94acbd49f9dd
marklubarsky <marklubarsky@gmail.com>
2011-01-18 16:12:41 -0800

    flatten the test suite to parallelize test cases on a method level

--- lib/parallel_tests/queued_test_suite.rb
+++ lib/parallel_tests/queued_test_suite.rb
@@ -11,18 +11,28 @@ module QueuedTestSuite
       DRb.start_service
       master = DRbObject.new(nil, "druby://127.0.0.1:1338")
       
-      puts "# of TESTS:#{@tests.size}, PROCESSOR ##{ENV['TEST_ENV_NUMBER']}"  
+      puts "# of TESTS:#{@tests.size}, FLATTENED:#{flatten.size} PROCESSOR ##{ENV['TEST_ENV_NUMBER']}"  
       # debugger
       
-      master.run_tests_later(@tests, ENV['TEST_ENV_NUMBER'].to_i)             
+      # puts "RUN TESTS LATER:#{@tests.inspect}"
+      master.run_tests_later(flatten, ENV['TEST_ENV_NUMBER'].to_i)             
                   
       while master.has_more? 
-        test_case = master.next        
-        test_case.run(result, &progress_block)            
+        test_case = master.next
+        test_case.run(result,&progress_block)            
         puts "run #{test_case.name}"   
       end
 
       yield(FINISHED, name)
     end
+    
+    def flatten
+      flattened_test_cases = @tests.inject([]) do |test_cases, test|
+        test_cases.concat test.tests if test.respond_to? :tests
+      end    
+      # puts "BEFORE flatten:#{@tests.map {|t| t.name}}"
+      # puts "AFTER flatten:#{flattened_test_cases.map {|t| t.name}}"
+      flattened_test_cases
+    end  
   end  
 end
