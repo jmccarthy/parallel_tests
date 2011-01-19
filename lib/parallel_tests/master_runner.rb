ff6044159545ee55b3ee6cd227e1e27e6f6330b6
marklubarsky <marklubarsky@gmail.com>
2011-01-18 16:51:00 -0800

    synchronization added

--- lib/parallel_tests/master_runner.rb
+++ lib/parallel_tests/master_runner.rb
@@ -1,14 +1,16 @@
 require 'drb'
 require 'drb/observer'
+require 'monitor'
 
 $LOAD_PATH << "test"
 
-class MasterRunner
+class MasterRunner < Monitor
 
   def initialize
     @queue = Array.new
     @tests_registered = false
     @tests_loaded = false
+    super
   end  
 
   def run_later(test_case, process_number)
@@ -19,31 +21,35 @@ class MasterRunner
       puts "skipping #{test_case.name} for processor ##{[process_number]}, QUEUE SIZE:#{@queue.size}"
     end
   end
-  
+
   def run_tests_later(tests, process_number)
     # TODO: synchronize this and release only when loaded
-    if first_process?(process_number) && !@tests_registered
-      tests.each do |test_case|
-        run_later(test_case, process_number)       
-        puts "enqueued #{test_case.name}"      
-      end        
-      close_queue(process_number)
-    else
-      # TODO: needs synchronization on tests_registered
-      
-      # puts "wait until all tests are registered, processor ##{process_number}, QUEUE SIZE:#{@queue.size}"
-      #       #return only if registered already, TODO: needs synchronization
-      #       while !tests_registered?; sleep 1; end
-      #       puts "wait over for processor ##{process_number}, QUEUE SIZE:#{@queue.size}"
-    end    
+    synchronize do
+      if first_process?(process_number) && !@tests_registered
+        tests.each do |test_case|
+          run_later(test_case, process_number)       
+          puts "enqueued #{test_case.name}"      
+        end        
+        close_queue(process_number)
+      else
+        # TODO: needs synchronization on tests_registered
+
+        # puts "wait until all tests are registered, processor ##{process_number}, QUEUE SIZE:#{@queue.size}"
+        #       #return only if registered already, TODO: needs synchronization
+        #       while !tests_registered?; sleep 1; end
+        #       puts "wait over for processor ##{process_number}, QUEUE SIZE:#{@queue.size}"
+      end    
+    end
   end  
-  
+
   # TODO: call from parallels_tests so that we require all tests and able to add them right here from master rather than from the 1st process
   def preload_tests(test_files)
-    return if @tests_loaded 
-    puts "REQUIRING FILES:#{test_files.inspect}"
-    test_files.each {|f| require f } 
-    @tests_loaded = true
+    synchronize do
+      return if @tests_loaded 
+      puts "REQUIRING FILES:#{test_files.inspect}"
+      test_files.each {|f| require f } 
+      @tests_loaded = true
+    end
   end  
 
   def close_queue(process_number)
