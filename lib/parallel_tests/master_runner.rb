0e30fe42acdd80bae163ff49ca9e304a17d283f2
marklubarsky <marklubarsky@gmail.com>
2011-01-21 17:02:05 -0800

    split the master queues into 2: flattened test cases and unflattened test suites for test suites with non-atomic test cases; created an env variable NONATOMIC_TEST_SUITES; test suites will run first guranteing the process untilization and then atomic test cases will kick in

--- lib/parallel_tests/master_runner.rb
+++ lib/parallel_tests/master_runner.rb
@@ -9,12 +9,21 @@ class MasterRunner
 
   def initialize
     @mutex = Mutex.new
-    @queue = Array.new
+    @test_case_queue = Array.new
+    @test_suite_queue = Array.new
     @tests_registered = false
     @registering_process_number = nil
   end  
-
-  def run_tests_later(tests, process_number)
+  
+  def run_test_suites_later(tests, process_number)
+    run_tests_later(tests, process_number, :test_suite_queue)
+  end  
+  
+  def run_test_cases_later(tests, process_number)
+    run_tests_later(tests, process_number, :test_case_queue)
+  end
+  
+  def run_tests_later(tests, process_number, queue_type)
     return nil if tests_registered?
     if @registering_process_number.nil? #store the very first process
       @mutex.synchronize do
@@ -23,22 +32,22 @@ class MasterRunner
     end
     if process_number.to_i == @registering_process_number.to_i #return if the caller is not a registering process
       tests.each do |test|
-        @queue << test
+        queue_type == :test_case_queue ? @test_case_queue << test : @test_suite_queue << test  
       end  
-      log_queue_size "run_tests_later(#{tests.size}, #{process_number})"
+      log_queue_size "run_tests_later(#{tests.size}, #{process_number}, #{queue_type})", queue_type
     else
-      log_queue_size "[skipping] run_tests_later(#{tests.size}, #{process_number})"
+      log_queue_size "[skipping] run_tests_later(#{tests.size}, #{process_number}, #{queue_type})", queue_type
     end
-  end  
+  end
 
-  def close_queue(process_number)
-    log_queue_size "close_queue(#{process_number}), registered by #{@registering_process_number}"
+  def close_queues(process_number)    
     if process_number.to_i == @registering_process_number.to_i #return if the caller is not a registering process
-      log_queue_size "close_queue(#{process_number})"
-      @queue = @queue.uniq
+      [:test_suite_queue, :test_case_queue].each {|q_type| log_queue_size "close_queue(#{process_number}), registered by #{@registering_process_number}", q_type }
+      @test_case_queue = @test_case_queue.uniq
+      @test_suite_queue = @test_suite_queue.uniq
       @tests_registered = true
     else
-      log_queue_size "[skipping] close_queue(#{process_number})"
+      [:test_suite_queue, :test_case_queue].each {|q_type| log_queue_size "close_queue(#{process_number})", q_type }
     end
   end  
 
@@ -46,32 +55,49 @@ class MasterRunner
     @tests_registered
   end 
 
-  def log_queue_size(caller, options={})
+  def log_queue_size(caller, queue_type, options={})
+    queue = (queue_type == :test_case_queue) ? @test_case_queue : @test_suite_queue
+    
     log_index = (options[:log_index] || 25).to_i        
-    show = options[:batch_size].to_i >= log_index ? true : @queue.size % log_index <= options[:batch_size].to_i  
+    show = options[:batch_size].to_i >= log_index ? true : queue.size % log_index <= options[:batch_size].to_i  
     show_max_test_suites = ENV['MAX_TEST_SUITES'].to_i
     show_max_test_cases = ENV['MAX_TEST_CASES_PER_SUITE'].to_i > 0 ? ENV['MAX_TEST_CASES_PER_SUITE'].to_i : 1    
     show ||= show_max_test_suites * show_max_test_cases <= log_index if show_max_test_suites * show_max_test_cases > 0
-    puts "[MASTER] QUEUE SIZE (#{caller}):#{@queue.size}" if show
+    puts "[MASTER] #{queue_type} QUEUE SIZE (#{caller}):#{queue.size}" if show
   end
 
-  def next
-    test_case = @queue.pop
-    log_queue_size 'pop', :batch_size => 1
-    test_case
+  def next_test_cases(limit)
+    next_batch(:test_case_queue, limit)
+  end
+  
+  def next_test_suites(limit)
+    next_batch(:test_suite_queue, limit)
   end  
 
-  def next_batch(limit=1)
+  def next_batch(queue_type, limit=1)
     batch = []
     limit = 1 if limit == 0
     limit.times do 
-      batch << @queue.pop if has_more? 
+      if queue_type == :test_case_queue
+        batch << @test_case_queue.pop if has_more_test_cases? 
+      else
+        batch << @test_suite_queue.pop if has_more_test_suites? 
+      end        
+
     end
-    log_queue_size "next_batch(#{limit})", :batch_size => limit  
+    log_queue_size "next_batch(#{queue_type}, #{limit})", queue_type, :batch_size => limit  
     batch
   end
   
-  def has_more?
-    !@queue.empty?
+  def has_more_test_cases?
+    has_more?(:test_case_queue)
+  end
+  
+  def has_more_test_suites?
+    has_more?(:test_suite_queue)
   end      
+  
+  def has_more?(queue_type)
+    queue_type == :test_case_queue ? !@test_case_queue.empty? : !@test_suite_queue.empty?
+  end
 end  
--- lib/parallel_tests/queued_test_suite.rb
+++ lib/parallel_tests/queued_test_suite.rb
@@ -20,79 +20,85 @@ module QueuedTestSuite
   class Test::Unit::TestSuite
     def run(result, &progress_block)
 
-
-      #TEST
       @tests = @tests[-ENV['MAX_TEST_SUITES'].to_i..-1] if ENV['MAX_TEST_SUITES'].to_i <= @tests.size #for debugging
 
       yield(STARTED, name)
 
       @process_number = ENV['TEST_ENV_NUMBER']
+      unflattened_test_suites = ENV['NONATOMIC_TEST_SUITES'].to_a.empty? ? [] : ENV['NONATOMIC_TEST_SUITES'].split(',') 
 
-      @flattened_tests = flatten_tests
+      @flattened_test_cases, @unflattened_test_suites = flatten_tests(@tests, unflattened_test_suites)
+
+      puts "FLATTENED TEST CASES: #{@flattened_test_cases.keys.inspect}"
+      puts "UNFLATTENED TEST SUITES: #{@unflattened_test_suites.keys.inspect}"
 
       DRb.start_service
       @master = DRbObject.new(nil, MasterRunner::URL)
 
-      puts "# of TESTS:#{@tests.size}, FLATTENED:#{@flattened_tests.size}, PROCESSOR ##{@process_number}"  
-      # debugger
+      puts "# of TESTS:#{@tests.size}, FLATTENED:#{@flattened_test_cases.size}, PROCESSOR ##{@process_number}"  
 
       # wait until either some other test process registers tests or if noone yet started registering (has_more? == false)
-      do_with_log('WAITED before entering queue') { while @master.has_more? && !@master.tests_registered?; sleep 1; end }
+      do_with_log('WAITED before entering queue') { while @master.has_more_test_suites? && @master.has_more_test_cases? && !@master.tests_registered?; sleep 1; end }
 
-      if !@master.tests_registered?
-        do_with_log("run later #{@flattened_tests.keys.size} tests") { @master.run_tests_later(@flattened_tests.keys, @process_number) }
-        do_with_log("closing queue") { @master.close_queue(@process_number) }
-
-        do_with_log('WAITED after entering queue') { while !@master.tests_registered?; sleep 1; end }
-      end
-
-      all_test_names = [] 
-      per_batch =  ENV['TESTS_BATCH_SIZE'].to_i
-      while @master.has_more?
-        tests_to_run = do_with_log("next batch of #{per_batch} ", {:modifier => :inspect}) { @master.next_batch(per_batch) }
-        all_test_names.concat tests_to_run
-        
-        # do_with_log("run now #{tests_to_run.size} tests") do
-          tests_to_run.each do |test_to_run|
-            run_now(test_to_run, result, &progress_block)        
-          end  
-        # end
-      end
-
-      puts "[PROCESS ##{@process_number}] FINISHED RUNNING #{all_test_names.size} tests"     
+      enqueue_for_later
+      run_now(:test_suite_queue, result, &progress_block)
+      run_now(:test_case_queue, result, &progress_block)
 
       yield(FINISHED, name)
     end
 
-    def run_now(test_case_name, result, &progress_block)
-      test_case = @flattened_tests[test_case_name]
-
-      if test_case
-        test_case.run(result, &progress_block)
-      else
-        msg = "ERROR: [PROCESS ##{@process_number}] UNABLE to find #{test_case_name}, skipping ..."
-        puts msg       
-      end  
-    end          
-
-    def run_later(test_case_name)
-      t_start = Time.now      
-      @master.run_later(test_case_name, @process_number)
-      t_end = Time.now
-      # puts "[PROCESS ##{@process_number}] run later #{test_case_name}, time took: #{t_end - t_start} seconds" 
-    end     
-
-    def flatten_tests
-      @tests.inject(ActiveSupport::OrderedHash.new) do |flattened_test_cases, test|
-        if test.respond_to? :tests
-          adj_tests = ENV['MAX_TEST_CASES_PER_SUITE'].to_i <= test.tests.size ? test.tests[-ENV['MAX_TEST_CASES_PER_SUITE'].to_i..-1] : test.tests 
-          adj_tests.each do |t|
-            flattened_test_cases[t.name] = t
-          end  
-        else
-          flattened_test_cases[test.name] = test
+    def enqueue_for_later
+      if !@master.tests_registered?
+        [:test_suite_queue, :test_case_queue].each do |queue_type|
+          tests_to_enqueue = (queue_type == :test_case_queue) ? @flattened_test_cases.keys : @unflattened_test_suites.keys
+          do_with_log("push_for_later(#{queue_type}) #{tests_to_enqueue.size} tests") { @master.run_tests_later(tests_to_enqueue, @process_number, queue_type) }
         end
-        flattened_test_cases
+        do_with_log("closing queue") { @master.close_queues(@process_number) }
+
+        do_with_log('WAITED after entering queue') { while !@master.tests_registered?; sleep 1; end }
+      end
+    end  
+
+    def run_now(queue_type, result, &progress_block)
+      all_test_names = [] 
+      per_batch =  queue_type == :test_case_queue ? ENV['TESTS_BATCH_SIZE'].to_i : 1
+      while @master.has_more?(queue_type)
+        test_names_to_run = do_with_log("next batch of #{per_batch} #{queue_type}", {:modifier => :inspect}) { @master.next_batch(queue_type, per_batch) }
+      
+        tests_to_run = if queue_type == :test_case_queue
+          @flattened_test_cases.slice(*test_names_to_run).values
+        else  
+          test_suites_to_run = @unflattened_test_suites.slice(*test_names_to_run).values
+          flatten_tests(test_suites_to_run, [])[0].values          
+        end  
+
+        all_test_names.concat tests_to_run.map(&:name)
+
+        tests_to_run.each do |test_case|          
+          if test_case
+            test_case.run(result, &progress_block)
+          else
+            puts "[PROCESS ##{@process_number}] ERROR: UNABLE TO FIND #{test_to_run} )"  
+          end
+        end
+      end
+
+      puts "[PROCESS ##{@process_number}] FINISHED RUNNING #{all_test_names.size} #{queue_type})"
+    end  
+
+    def flatten_tests(test_suites, exclude_list)
+      test_suites.inject([ActiveSupport::OrderedHash.new, ActiveSupport::OrderedHash.new]) do |flattened_test_cases_or_unflattened_suites, test|
+        if test.respond_to?(:tests)
+          adj_tests = ENV['MAX_TEST_CASES_PER_SUITE'].to_i <= test.tests.size ? test.tests[-ENV['MAX_TEST_CASES_PER_SUITE'].to_i..-1] : test.tests 
+          if !exclude_list.include?(test.name)
+            adj_tests.each do |t|            
+              flattened_test_cases_or_unflattened_suites[0][t.name] = t
+            end  
+          else
+            flattened_test_cases_or_unflattened_suites[1][test.name] = test
+          end
+        end
+        flattened_test_cases_or_unflattened_suites #returns [flattened cases, unflattened suites]
       end    
     end  
 
